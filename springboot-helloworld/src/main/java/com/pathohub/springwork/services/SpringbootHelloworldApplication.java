f063b3b2ca735097289469f61d7111bd777869e9
prathap2k <Cheekaboy@192.168.0.4>
2016-10-06 22:58:12 -0500

    hawtio Change-Id: I5df9b94cd74876e755ed2087460bbaec301f311e Signed-off-by: prathap2k

--- springboot-helloworld/src/main/java/com/pathohub/springwork/services/SpringbootHelloworldApplication.java
+++ springboot-helloworld/src/main/java/com/pathohub/springwork/services/SpringbootHelloworldApplication.java
@@ -2,23 +2,57 @@ package com.pathohub.springwork.services;
 
 //import java.util.Arrays;
 
+import io.hawt.springboot.EnableHawtio;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import io.hawt.config.ConfigFacade;
+import io.hawt.springboot.EnableHawtio;
+import io.hawt.system.ConfigManager;
+import io.hawt.web.AuthenticationFilter;
 
 @SpringBootApplication
+@EnableHawtio
 public class SpringbootHelloworldApplication {
 
 	public static void main(String[] args) {
-		
+		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
 		SpringApplication.run(SpringbootHelloworldApplication.class, args);
 	  //ApplicationContext ctx = SpringApplication.run(SpringbootHelloworldApplication.class, args);
-       /* System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-	}*/
+	}
+	
+	
+	@Autowired
+	private ServletContext servletContext;
+	
+	@PostConstruct
+	public void init() {
+		final ConfigManager configManager = new ConfigManager();
+		configManager.init();
+		servletContext.setAttribute("ConfigManager", configManager);
+	}
+	
+	/**
+	 * Set things up to be in offline mode
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public ConfigFacade configFacade() throws Exception {
+		ConfigFacade config = new ConfigFacade() {
+			public boolean isOffline() {
+				return true;
+			}
+		};
+		config.init();
+		return config;
 	}
 }
