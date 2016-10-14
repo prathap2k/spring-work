fc6adced25178af36266fa3ee4981cb8a6eb747a
pthotapalle <pthotapalle@USBABTZ9FX1.us.aegon.com>
2016-10-14 16:11:57 -0500

    PHSB-0001

---
+++ pathohub-springboot-startup/src/main/java/com/pathohub/services/SpringbootStartupApplication.java
@@ -0,0 +1,13 @@
+package com.pathohub.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStartupApplication.class, args);
+	}
+}
